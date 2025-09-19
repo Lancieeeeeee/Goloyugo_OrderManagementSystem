342f8923508597ac5ad34df982ae1bfdefdb81a0
Lancieeeeeee <jd7730365@gmail.com>
2025-09-19 10:01:49 +0800

    commit

--- Goloyugo_OrderManagementSystem/Form1.Designer.vb
+++ Goloyugo_OrderManagementSystem/Form1.Designer.vb
@@ -80,7 +80,7 @@ Partial Class Form1
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(463, 280)
+        Me.Button1.Location = New System.Drawing.Point(465, 269)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 5
